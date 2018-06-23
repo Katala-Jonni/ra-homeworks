58c1b1827f1617bcc2fd74e386b977adbfb487de
Max <katala.jonni@yandex.ru>
2018-06-23 06:31:36 +0300

    0.0.4

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,39 @@
-'use strict'
+'use strict';
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: 'All',
+            project: this.props.projects
+        }
+    }
+
+    getProject(filter) {
+        const copyProjects = this.props.projects.slice();
+        let filterProject = copyProjects.filter(el => el.category === filter);
+        if (!filterProject.length) {
+            filterProject = copyProjects;
+        }
+        return filterProject;
+    }
+
+    handle(filter) {
+        this.setState({
+            active: filter,
+            project: this.getProject(filter)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Toolbar
+                    filters={this.props.filters}
+                    selected={this.state.active}
+                    onSelectFilter={(filter) => this.handle(filter)}/>
+                <Portfolio projects={this.state.project}/>
+            </div>
+        );
+    }
+}
--- state/layouts/js/App.js
+++ state/layouts/js/App.js
@@ -4,44 +4,63 @@ const VIEW_LIST = "view_list";
 const VIEW_MODULE = "view_module";
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="toolbar">
-          <IconSwitch
-            icon={VIEW_MODULE}
-            onSwitch={() => console.log("сменился тип вывода")} />
-        </div>
-        {this.renderLayout(true)}
-      </div>
-    );
-  }
-
-  renderLayout(cardView) {
-    if (cardView) {
-      return (
-        <CardsView
-          layout={this.props.layout}
-          cards={this.getShopItems(this.props.products, cardView)} />
-      );
+    constructor(props) {
+        super(props);
+        this.state = {
+            switch: VIEW_MODULE,
+            isCardView: true
+        };
     }
-    return (<ListView items={this.getShopItems(this.props.products, cardView)} />);
-  }
 
-  getShopItems(products, cardView) {
-    return products.map(product => {
-      let cardProps = {
-        title: product.name,
-        caption: product.color,
-        img: product.img,
-        price: `$${product.price}`
-      };
-      if (cardView) {
+    getSwitch(event) {
+        return [VIEW_MODULE, VIEW_LIST].find(el => el !== event.target.textContent);
+    }
+
+    handle(event) {
+        this.setState({
+            switch: this.getSwitch(event),
+            isCardView: !this.state.isCardView
+        })
+    }
+
+    render() {
         return (
-          <ShopCard {...cardProps}/>
+            <div>
+                <div className="toolbar">
+                    <IconSwitch
+                        icon={this.state.switch}
+                        onSwitch={() => this.handle(event)}/>
+                </div>
+                {this.renderLayout(this.state.isCardView)}
+            </div>
         );
-      }
-      return (<ShopItem {...cardProps}/>)
-    });
-  }
+    }
+
+    renderLayout(cardView) {
+        if (cardView) {
+            return (
+                <CardsView
+                    layout={this.props.layout}
+                    cards={this.getShopItems(this.props.products, cardView)}/>
+            );
+        }
+        return (<ListView items={this.getShopItems(this.props.products, cardView)}/>);
+    }
+
+    getShopItems(products, cardView) {
+        return products.map(product => {
+            let cardProps = {
+                title: product.name,
+                caption: product.color,
+                img: product.img,
+                price: `$${product.price}`
+            };
+            if (cardView) {
+                return (
+                    <ShopCard {...cardProps}/>
+                );
+            }
+            return (<ShopItem {...cardProps}/>)
+        });
+    }
 }
